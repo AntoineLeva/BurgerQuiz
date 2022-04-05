2fc25840b1a0658ac7b483f9a1d20bfcc47f68e3
Allan Ponchaut <allan_ponchaut@ens.univ-artois.fr>
2022-04-05 13:32:59 +0200

    partie selection du mode terminé

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs';
 import {Question} from '../models/question';
 import {QuestionsService} from '../services/questions.service';
 import {logger} from 'codelyzer/util/logger';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Reponse} from '../models/reponse';
 import {ReponsesService} from '../services/reponses.service';
 
@@ -15,10 +15,10 @@ import {ReponsesService} from '../services/reponses.service';
 export class QuestionsComponent implements OnInit {
   question: Question = {} as Question;
   reponses: Reponse[] = [];
-  loading: boolean = false;
-  private goodPoint: number =0;
+  loading = false;
+  private goodPoint = 0;
 
-  constructor(private router: Router,private questionsService: QuestionsService, private route: ActivatedRoute, private reponsesService: ReponsesService) { }
+  constructor(private router: Router, private questionsService: QuestionsService, private route: ActivatedRoute, private reponsesService: ReponsesService) { }
 
   ngOnInit(): void {
     this.loading = true;
@@ -30,26 +30,26 @@ export class QuestionsComponent implements OnInit {
         this.loading = false;
       });
     });
-    
+
   }
 
-  isGoodAnswers(reponse:Reponse){
-    if(reponse.bonne_reponse){
+  isGoodAnswers(reponse: Reponse){
+    if (reponse.bonne_reponse){
       this.goodPoint += 1;
     }
     console.log(this.goodPoint);
     let id = +(this.route.snapshot.paramMap.get('id') || 0);
-    this.questionsService.getQuestion(id+1).subscribe(question => {
+    this.questionsService.getQuestion(id + 1).subscribe(question => {
       this.question = question;
-      this.reponsesService.getReponses(id+1).subscribe(reponses => {
+      this.reponsesService.getReponses(id + 1).subscribe(reponses => {
         this.reponses = reponses;
         this.loading = false;
         id++;
-        this.router.navigate(['/question',id])
+        this.router.navigate(['/question', id]);
       });
     });
 
 
-    
+
   }
 }
