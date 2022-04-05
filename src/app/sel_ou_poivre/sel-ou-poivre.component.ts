217fe14d0607f6fedf0f0b452d5ad2ccd7507271
Leva Antoine <antoine_leva@ens.univ-artois.fr>
2022-04-05 11:08:58 +0200

    Correction

--- src/app/sel_ou_poivre/sel-ou-poivre.component.ts
+++ src/app/sel_ou_poivre/sel-ou-poivre.component.ts
@@ -3,27 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sel-ou-poivre',
   template: `
-<<<<<<< HEAD
     <mat-card [routerLink]="['/question/7']">
       <mat-card-header>
         <mat-card-title>Sel ou Poivre :</mat-card-title>
       </mat-card-header>
       <img src="assets/images/sel_ou_poivre.jpg" width="1000"/>
     </mat-card>
-    
   `,
-  styles: [
-=======
-      <mat-card [routerLink]="['/question/7']">
-        <mat-card-header>
-          <mat-card-title>Sel ou Poivre :</mat-card-title>
-        </mat-card-header>
-        <img mat-card-image src="assets/images/sel_ou_poivre.png">
-      </mat-card>
-  `,
+
   styles: [
     'img{height: 78vh;  display: flex; flex-direction: column; align-items: center; justify-content: center; background-size: cover; font-size: 30rem; }'
->>>>>>> origin/antoineL
   ]
 })
 export class SelOuPoivreComponent implements OnInit {
